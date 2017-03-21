a4f51b36c57a498f5b92729eaf022631637fa52a
Justine Chen <justinechen619@gmail.com>
2017-03-21 16:38:31 -0400

    Wrong formula but somewhat working

--- nulogyexercise.java
+++ nulogyexercise.java
@@ -3,11 +3,31 @@ public class nulogyexercise {
 	/**
  *  Calculate the pricing
  */
-	//  public float calculateMarkUp(float basePrice, int numPeople, String jobType){
+	 public static double calculateMarkUp(double basePrice, int numPeople, String jobType){
+	 	double material = 0;
+	 	if(jobType == "food"){
+	 		material += 0.13;
+	 	}
 
-	// }
+	 	else if(jobType == "drugs"){
+	 		material += 0.075;
+	 	}
+
+	 	else if(jobType == "electronics"){
+	 		material += 0.02;
+	 	}
+	 	double markUp = 0.05 
+	 				   + (0.012 * numPeople)
+	 				   + material 
+	 				   + 1.0;
+	 	return markUp * basePrice;
+
+	}
 	public static void main(String[] args){
-	System.out.println(args[0]);
+		//Need to deal with commandline input to put into function
+	// System.out.println(args[0]);
+		double toPrint = calculateMarkUp(1299.99, 3, "food");
+		System.out.println(toPrint);
 }
 }
 
