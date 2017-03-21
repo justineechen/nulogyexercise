d5ee53d0c0f84c555f7132fe315a2ddca99df8ba
Justine Chen <justinechen619@gmail.com>
2017-03-21 13:00:45 -0400

    Coding on sublime now instead of intellij

---
+++ nulogyexercise.java
@@ -0,0 +1,15 @@
+
+public class nulogyexercise {
+	/**
+ *  Calculate the pricing
+ */
+	//  public float calculateMarkUp(float basePrice, int numPeople, String jobType){
+
+	// }
+	public static void main(String[] args){
+	System.out.println(args[0]);
+}
+}
+
+
+
